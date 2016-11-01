cd4643d686ebef4a016feee78791b7259e1124bd
Michael Hewson <michael@michaelhewson.ca>
2016-11-01 00:05:02 -0400

    got it compiling with xargo

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+#![feature(lang_items)]
+#![no_std]
+
+#[lang = "eh_personality"]
+extern fn eh_personality() {
+}
+
+#[lang = "panic_fmt"]
+extern fn rust_begin_panic() -> ! {
+    loop {}
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
